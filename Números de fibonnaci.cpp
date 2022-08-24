63abee9977dee59e75adeaa57b52f01b63bc6318
Francisco pedro <franciscopedroficial@gmail.com>
2022-08-24 13:46:20 -0300

    qualquer coisa

---
+++ Números de fibonnaci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n){
+	int fib=0;
+	if(n==0){ 
+		fib=0;
+		return fib;
+		}
+	if(n==1 || n==2){
+		fib=1;
+		return fib;
+	}
+	else{
+		return (fibonacci(n-1)+fibonacci(n-2));
+	}
+	
+}
+
+
+int main(){
+	int n;
+	cin>> n;
+	cout << fibonacci(n);
+
+}
