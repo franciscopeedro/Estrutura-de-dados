7235b000ff728cbb102e69af2249ebc101911f2e
Francisco Pedro <109813896+franciscopeedro@users.noreply.github.com>
2022-10-08 13:45:26 -0300

    finish

--- Divisão decimais.cpp
+++ Divisão decimais.cpp
@@ -7,7 +7,7 @@ int main(){
     cin >> a >> b;
     cout << "O quociente: " << a/b << endl;
     cout << "O resto: " << a%b << endl;
-    cout << "O resultando com decimais: " << fixed << setprecision(2) << float(a) /b  << endl;
+    cout << "O resultando com decimais: "<< fixed << setprecision(2) << float(a) /b  << endl;
 
 }
 
