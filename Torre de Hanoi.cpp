4d0c8a28c959193e81f13cf32838d4e9e96a9ea3
Francisco Pedro <109813896+franciscopeedro@users.noreply.github.com>
2022-10-13 18:26:03 -0300

    Finish

--- Torre de Hanoi.cpp
+++ Torre de Hanoi.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-void hanoi(int n, char a, char b, char c){
+void hanoi(int n,char a, char b, char c){
     if(n>=1){
         hanoi(n-1,a,c,b);
         cout << a << "->" << c << endl;
